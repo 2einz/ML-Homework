8bfe4781b540859ca637a17381ebb5a683a735b2
2einz <reinl@qq.com>
2025-05-06 20:04:40 +0800

    Ex7 kmeans

---
+++ Ex7-kmeans & PCA/utils.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+import seaborn as sns
+import pandas as pd
+from sklearn.cluster import KMeans
+from skimage import io
