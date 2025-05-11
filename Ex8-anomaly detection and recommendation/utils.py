5ef80c6676c552aae576600af54021330040e9d8
2einz <reinl@qq.com>
2025-05-11 22:05:50 +0800

    ex8

---
+++ Ex8-anomaly detection and recommendation/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, classification_report
+from scipy import stats, optimize
